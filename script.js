22e2588b91407f3fba859e988b06353c9715cb65
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-27 09:26:08 +0300

    update 1 script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const amount = document.getElementById('amount');
 const convertButton = document.getElementById('convert-button');
 const resultDiv = document.getElementById('result');
 const currencyList =    document.getElementById('currency-list');
+
+// Populate dropdowns
+fetch(`https://v6.exchangerate-api.com/v6/3158aa5a0cd960475500cb1e/latest/USD`)
+    .then(response => response.json())
+    .then(data => {
+        const currencies = Object.keys(data.conversion_rates);
+        currencies.forEach(currency => {
+            fromCurrency.add(new Option(currency, currency));
+            toCurrency.add(new Option(currency, currency));
+        });
+    });
