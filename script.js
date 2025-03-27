a277a6120c47b931886ce7f6c94d81736a3a8b8f
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-27 09:22:46 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const fromCurrency = document.getElementById('fromCurrency');
+const toCurrency = document.getElementById('toCurrency');
+const amount = document.getElementById('amount');
+const convertButton = document.getElementById('convert-button');
+const resultDiv = document.getElementById('result');
+const currencyList =    document.getElementById('currency-list');
